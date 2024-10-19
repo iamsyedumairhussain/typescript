20cd14ecde16932c3296b6d52f86ce8a950ffdac
Syed Umair Hussain <142947133+iamsyedumairhussain@users.noreply.github.com>
2024-10-20 02:17:21 +0500

    Create functions.ts

---
+++ functions.ts
@@ -0,0 +1,13 @@
+function addTwo(num:number): string {
+    return 'hello'
+}
+
+// arrow functions
+
+
+const addTwo = (num:number):string => {
+    return "hello"
+}
+
+const data = addTwo(10)
+export {}
