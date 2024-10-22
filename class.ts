8edaf66a2ecb2b4c1d1f021194953914698cad0b
Syed Umair Hussain <142947133+iamsyedumairhussain@users.noreply.github.com>
2024-10-22 18:33:23 +0500

    Create class.ts

---
+++ class.ts
@@ -0,0 +1,16 @@
+class User {
+    name: string
+    age: number
+    readonly dept:string
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+        this.dept = 'CS'
+    }
+}
+
+const user1 = new User('Umair', 21);
+console.log('user1 =======>', user1)
+
+
+export { }
