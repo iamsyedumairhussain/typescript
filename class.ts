f055d0b02e3c283e780a16978a63ff3e743befbd
Syed Umair Hussain <142947133+iamsyedumairhussain@users.noreply.github.com>
2024-10-22 18:37:52 +0500

    Update class.ts

--- class.ts
+++ class.ts
@@ -13,4 +13,21 @@ const user1 = new User('Umair', 21);
 console.log('user1 =======>', user1)
 
 
+// ================ shorter syntax
+
+class User {
+    constructor(public name: string, public age: number, readonly dept?: string) {
+        this.name = name
+        this.age = age
+        this.dept = 'CS'
+    }
+}
+
+const user1 = new User('Umair', 21);
+console.log('user1 =======>', user1)
+
+
+export { }
+
+
 export { }
